1c5a8d9fc63d008000df81a327451a5681930067
ANarngAung <anarngaung2-17@gmail.com>
2024-08-07 07:16:52 +0630

    added define valid options at script

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,10 @@ document.querySelector('#submit').addEventListener('click', function(heart) {
     // Get the value from the input field
     var inputValue = document.querySelector('#heartInput').value;
 
-   
+    // Define the valid options
+    var validOptions = ["1500", "528", "143", "Love", "180", "Ko", "koko", "I Love You", "I love you", "love", "baby", "Baby", 
+        "၁၅၀၀", "၅၂၈", "ကို", "ကိုကို", "မောင်", "မောင်လေး", "ဦး", "ဦးဦး", "ချစ်", "ချစ်ချစ်", "ချစ်လေး", "မင်းကို ချစ်တယ်", "မင်းကိုချစ်တယ်", "ချစ်တယ်"];
+
+    
 
 });
