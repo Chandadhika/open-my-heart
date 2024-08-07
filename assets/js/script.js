857b09cf1970f52b8f01e4e4f84d393350eaed14
ANarngAung <anarngaung2-17@gmail.com>
2024-08-07 07:59:10 +0630

    added trim function on script

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,11 +6,11 @@ document.querySelector('#submit').addEventListener('click', (heart) => {
     var inputValue = document.querySelector('#heartInput').value;
 
     // Define the valid options
-    var validOptions = ["1500", "528", "143", "Love", "180", "Ko", "koko", "I Love You", "I love you", "love", "baby", "Baby", 
-        "၁၅၀၀", "၅၂၈", "ကို", "ကိုကို", "မောင်", "မောင်လေး", "ဦး", "ဦးဦး", "ချစ်", "ချစ်ချစ်", "ချစ်လေး", "မင်းကို ချစ်တယ်", "မင်းကိုချစ်တယ်", "ချစ်တယ်"];
+    var validOptions = ["1500", "528", "143", "Love", "180", "9", "99", "Ko", "ko", "koko", "I Love You", "I love you", "love", "baby", "Baby", 
+        "၁၅၀၀", "၅၂၈", "၁၄၃", "၁၈၀", "၉", "၉၉", "ကို", "ကိုကို", "မောင်", "မောင်လေး", "ဦး", "ဦးဦး", "ချစ်", "ချစ်ချစ်", "ချစ်လေး", "မင်းကို ချစ်တယ်", "မင်းကိုချစ်တယ်", "ချစ်တယ်"];
 
     // Check if the input value matches any of the valid options
-    if (validOptions.includes(inputValue)) {
+    if (validOptions.includes(inputValue.trim())) {
         document.querySelector('#result').textContent = "💝  I love You  💞";
     } else {
         document.querySelector('#result').textContent = "Don't you love me?...💔 💔 💔 💔 💔";
