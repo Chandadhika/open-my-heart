adc9ad7ef6f40fd2692a5e698f19de35b11075f1
ANarngAung <anarngaung2-17@gmail.com>
2024-08-07 07:13:54 +0630

    added the value for input

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,9 @@ document.querySelector('#submit').addEventListener('click', function(heart) {
     // Prevent form submission
     heart.preventDefault();
 
+    // Get the value from the input field
+    var inputValue = document.querySelector('#heartInput').value;
+
    
 
 });
